2e49c1bf51709cfe2aa7cab60378fbd4cc2b68db
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-28 14:58:39 +0900

    add generate JWT token

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const { body, validationResult } = require('express-validator');
 const User = require("../db/User");
+const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+
+require('dotenv').config();
 
 router.get('/', (req, res) => {
     res.send("Hello Authjs")
@@ -11,7 +15,7 @@ router.post("/register",
     // バリデーションチェック
     body("email").isEmail(),
     body("password").isLength({ min: 6 }),
-    (req, res) => {
+    async (req, res) => {
         const email = req.body.email;
         const password = req.body.password;
 
@@ -31,7 +35,35 @@ router.post("/register",
         }
 
         // パスワードのハッシュ化
+        let hashedPassword = await bcrypt.hash(password, 10);
+        // console.log(hashedPassword)
+
+        // db保存
+        User.push({
+            email,
+            password:hashedPassword
+        })
+        
+        // JWT発行
+        const token = await jwt.sign(
+            {
+                email,
+            },
+            process.env.SECRET_KEY,
+            {
+                expiresIn:"24h",
+            }
+        )
+
+        return res.json({
+            token:token,
+        });
     }
 )
+
+// DB確認api
+router.get("/allUsers", (req, res) => {
+    return res.json(User);
+})
 
 module.exports = router;
