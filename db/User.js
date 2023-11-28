846a9d5dca51ccdfb1578ca852078f96512c9691
kazu0429 <k.shouhi0412@i.softbank.jp>
2023-11-28 14:23:14 +0900

    add email validate & db check

---
+++ db/User.js
@@ -0,0 +1,8 @@
+const User = [
+    {
+        email:"k3394902@kadai.jp",
+        password:"123456789"
+    }
+];
+
+module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,37 @@
 const router = require("express").Router();
+const { body, validationResult } = require('express-validator');
+const User = require("../db/User");
 
 router.get('/', (req, res) => {
     res.send("Hello Authjs")
 })
 
-router.post("/register", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+router.post("/register",
 
-    console.log(email, password);
-})
+    // バリデーションチェック
+    body("email").isEmail(),
+    body("password").isLength({ min: 6 }),
+    (req, res) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()})
+        }
+        
+        // DBにユーザーが存在しているか
+        const user = User.find((user) => user.email === email);
+        if(user){
+            return res.status(400).json([
+                {
+                    message:"すでにそのユーザーは存在しています。"
+                }
+            ])
+        }
+
+        // パスワードのハッシュ化
+    }
+)
 
 module.exports = router;
